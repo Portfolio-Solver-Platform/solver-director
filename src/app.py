224493b5eece918fe5eff2d74a9ad3387de2ff3f
Sofus Laubel <sofus@laubel.net>
2025-09-09 15:32:20 +0200

    small fixes

--- src/app.py
+++ src/app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 def root():
     return jsonify(status="ok", service="flask-helm-skaffold")
 
+
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=8080)
-    
+
     import os
+
     host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "8080"))
     app.run(host=host, port=port)
