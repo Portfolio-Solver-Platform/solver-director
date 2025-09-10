f1910de6c4eb976f370b8bfe49cee109defe8664
Jonas Bork <jonas10bork@gmail.com>
2025-09-10 11:17:55 +0200

    Add basic monitoring support

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 
 @app.route("/", methods=["GET"])
 def root():
     return jsonify(status="ok", service="flask-helm-skaffold")
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
